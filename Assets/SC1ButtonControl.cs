aac180d1d545191657adc5a2eb4e0e84a2c0188a
y_okada <yasokada@gmail.com>
2015-08-14 13:02:49 +0900

    SC1ButtonControl: extract method to SelectFunc()

--- Assets/SC1ButtonControl.cs
+++ Assets/SC1ButtonControl.cs
@@ -5,21 +5,33 @@ using UnityEngine.UI;
 public class SC1ButtonControl : MonoBehaviour {
 
 	private const int kFileId = 0; // change this for each scene
-	private const bool kDirSearch = true; // false:file selection, true:dir selection
+	private bool kDirSearch = false; // false:file selection, true:dir selection
 
 	public InputField dirNameInputField; // should be related to Input Field for entering dirname
 
-	public void Button1Click() {
+	private void SelectFunc(bool dirSearch)
+	{
+		kDirSearch = false;
+		
 		ScrollScript.SetDirSearch (kDirSearch);
 		bool res = ScrollScript.ReadFromDir (dirNameInputField.text);
 		if (res == false) {
 			Debug.Log("dir not found");
 			return;
 		}
-
+		
 		SelectButtonControl.SetCallingScene (Application.loadedLevelName);
 		SelectButtonControl.SetFileId (kFileId);
 		Application.LoadLevel ("FileSelector");
+	}
+
+	public void ButtonFileSelectClick() {
+		SelectFunc(/* dirSearch=*/false);
+	}
+
+	public void ButtonDirSelectClick()
+	{
+		SelectFunc(/* dirSearch=*/true);
 	}
 
 	void UpdateInputField (string dirname) {
