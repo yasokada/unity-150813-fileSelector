b5927d845b970cd385c412f00680512179d31a6b
y_okada <yasokada@gmail.com>
2015-08-14 10:20:58 +0900

    update using Application.loadedLevelName

--- Assets/SC2ButtonControl.cs
+++ Assets/SC2ButtonControl.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class SC2ButtonControl : MonoBehaviour {
-
+	
 	public void SC2ButtonClick()
 	{
-		SelectButtonControl.callingScene = "Scene2";
+		//SelectButtonControl.callingScene = "Scene2";
+		SelectButtonControl.callingScene = Application.loadedLevelName;
 		Application.LoadLevel ("FileSelector");
 	}
-
+	
 	public Text myText; // should be related to Text in the scene
 	void OnGUI() {
 		myText.text = SelectButtonControl.filename;
