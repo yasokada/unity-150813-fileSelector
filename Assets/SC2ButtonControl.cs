de2a009e54b7d5874990fec6f86453373b286031
y_okada <yasokada@gmail.com>
2015-08-14 09:45:11 +0900

    add Scene2

---
+++ Assets/SC2ButtonControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SC2ButtonControl : MonoBehaviour {
+
+	public void SC2ButtonClick()
+	{
+		SelectButtonControl.callingScene = "Scene2";
+		Application.LoadLevel ("FileSelector");
+	}
+
+	public Text myText; // should be related to Text in the scene
+	void OnGUI() {
+		myText.text = SelectButtonControl.filename;
+	}
+}
